8200d2f902495e92c2db05bb1cf9a2148604c6e9
Carsten van Weelden <cvanweelden@gmail.com>
2011-09-20 15:16:28 +0200

    Initial commit

---
+++ Apriori/src/apriori/Apriori.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apriori;
+
+/**
+ *
+ * @author Carsten van Weelden <cweelden@science.uva.nl>
+ */
+public class Apriori {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
